ebe93570cb565ba5a5752f0b39c9fa57d4f8aa6f
Dani M <rzqxx5gv@gmail.com>
2016-05-04 20:33:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,17 @@
 #DATE: 4.28.2016
 
 #PURPOSE:
-#This script moves the files to ytdl to the correct locatons, and changes the 
+#This script moves the files of ytdl to the correct locatons, and changes the 
 #permissions of the files in ~/Music so that users other than root can access them.
-#the script requires root because root permissions are needed to write to /bin.
-#This script installs ytdl v1.10+
+#root permissions are needed to write to /bin.
+#This script installs ytdl v1.10.
 
-#Version 1.00
+#Version 1.01
 #===================================================================================
+
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+wd=$(pwd)
 
 # Are we root?
 if [[ $EUID -ne 0 ]]; then
@@ -27,21 +31,25 @@ else
       #Ok, here's the magic.
       echo "creating $me/Music/ytdl-downloads"
       mkdir $me/Music/ytdl-downloads
+
       echo "setting permissions for '$me/Music/ytdl-downloads'"
       chmod a+rwx $me/Music/ytdl-downloads
 
       echo "moving help page to $me/Music/ytdl-downloads"  
       cp ytdl-help.txt $me/Music/ytdl-downloads
+
       echo "Changing permissions for '$me/ytdl-downloads/ytdl-help.txt'"
       chmod a+rwx $me/Music/ytdl-downloads/ytdl-help.txt
 
       echo "moving ytdl to /bin"
       mv ytdl.sh ytdl
       cp ytdl /bin
+      
+      echo "changing permissions of 'ytdl.sh'"
+      chmod a+rwx /bin/ytdl      
       mv ytdl ytdl.sh
 
       #ending message:
       tput setaf 2; echo -e "[ OK ] \c"
       tput sgr0   ; echo -e "Done!  Enjoy ytdl!"
 fi
-
