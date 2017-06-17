c49e7b47bbc12031115c97c76ede330bca981365
reprise5 <rzqxx5gv@gmail.com>
2017-06-16 21:56:00 -0400

    create .m3u entries, called from ytdl

--- install.sh
+++ install.sh
@@ -4,54 +4,92 @@
 #DATE: 4.28.2016
 
 #PURPOSE:
-#This script moves the files of ytdl to the correct locatons, and changes the 
+#This script moves the files of ytdl to the correct locatons, and changes the
 #permissions of the files in ~/Music so that users other than root can access them.
 #root permissions are needed to write to /usr/bin.
 #This script installs ytdl v1.10+.
 
-#Version 1.01
+#Version 1.3.0
 #===================================================================================
+opt=$1
+case "$opt" in
+      # RUN AS AN UPDATER (needs work)
+      -u|--update)
+            #need to be root for this too, because copying to /usr/bin.
+            if [[ $EUID -ne 0 ]]; then
+                  echo "This script must be run as root in order to write to /usr/bin." 1>&2
+                  exit 1
 
-FILE="/tmp/out.$$"
-GREP="/bin/grep"
-wd=$(pwd)
+            #Yes? Install time.
+            else
+                  #finding out what user we're installing to, since ~/ will return /home/root.
+                  me=$(readlink -f install.sh | cut -f3 -d '/')
+                  me=$(echo /home/$me)
 
-# Are we root?
-if [[ $EUID -ne 0 ]]; then
-      echo "This script must be run as root" 1>&2
-      exit 1
+                  #get files from github, then put the new copy of ytdl in place.
+                  cd $me/Desktop
+                  echo "Downloading..."
+                  git clone https://github.com/reprise5/ytdl
+                  cd ytdl
 
-#Yes? Install time.
-else   
-      #finding out what user we're installing to, since ~/ will return /home/root.
-      me=$(readlink -f install.sh | cut -f3 -d '/')
-      echo "Installing to user: '$me'"
-      me=$(echo /home/$me)
+                  echo "Giving execute permissions to'ytdl.sh'"
+                  chmod a+x ytdl.sh
 
-      #Ok, move files and set correct permissions.
-      echo "creating $me/Music/ytdl-downloads"
-      mkdir $me/Music/ytdl-downloads
+                  # Won't work unless YTDL can be run as root.
+                  installedVersion=$(ytdl --version | cut -d ' ' -f 3)
+                  thisVersion=$(./ytdl.sh --version)
+                  echo "installedVersion: $installedVersion thisVersion $thisVersion"
 
-      echo "setting permissions for '$me/Music/ytdl-downloads'"
-      chmod a+rwx $me/Music/ytdl-downloads
+                  if [[ thisVersion != installedVersion ]]; then
+                        echo "moving ytdl.sh to /usr/bin..."
+                        cp ytdl.sh /usr/bin
+                        mv /usr/bin/ytdl.sh /usr/bin/ytdl
+                  fi
 
-      echo "moving help page to $me/Music/ytdl-downloads"  
-      cp ytdl-help.txt $me/Music/ytdl-downloads
+                  cd ..
+                  rm -rf ytdl
+            fi
 
-      echo "Changing permissions for '$me/ytdl-downloads/ytdl-help.txt'"
-      chmod a+rwx $me/Music/ytdl-downloads/ytdl-help.txt
+            if [ -t 1 ]; then
+                  tput setaf 1; echo -e "[ !! ] \c"
+                  tput sgr0   ; echo -e "There was an error while updating ytdl."
 
-      echo "moving ytdl to /usr/bin"
-      mv ytdl.sh ytdl
-      cp ytdl /usr/bin
-      
-      echo "changing permissions of 'ytdl.sh'"
-      chmod a+rwx /usr/bin/ytdl      
+            else
+                  tput setaf 2; echo -e "[ OK ] \c"
+                  tput sgr0   ; echo -e "UP-TO-DATE"
+            fi
+            ;;
+      # RUN AS AN INSTALLER. (first time install)
+      *)
+            # Are we root?
+            if [[ $EUID -ne 0 ]]; then
+                  echo "This script must be run as root in order to write to /usr/bin." 1>&2
+                  exit 1
 
-      mv ytdl ytdl.sh
+            #Yes? Install time.
+            else
+                  # Finding out what user we're installing to, since ~/ will return /home/root.
+                  me=$(readlink -f install.sh | cut -f3 -d '/')
+                  me=$(echo /home/$me)
 
-      #ending message:
-      tput setaf 2; echo -e "[ OK ] \c"
-      tput sgr0   ; echo -e "Done!  Enjoy ytdl!"
-fi
+                  #Ok, move files and set correct permissions.
+                  echo "creating $me/Music/ytdl-downloads"
+                  mkdir $me/Music/ytdl-downloads
 
+                  echo "setting permissions for '$me/Music/ytdl-downloads'"
+                  chmod a+rwx $me/Music/ytdl-downloads
+
+                  echo "moving ytdl & playlist to /usr/bin"
+                  mv ytdl.sh /usr/bin/ytdl
+                  mv playlist.py /usr/bin/playlist
+
+                  echo "Giving execute permissions to'ytdl.sh' and 'playlist.py'."
+                  chmod a+x /usr/bin/ytdl
+                  chmod a+x /usr/bin/playlist
+
+                  #ending message:
+                  tput setaf 2; echo -e "[ OK ] \c"
+                  tput sgr0   ; echo -e "Done!  Enjoy ytdl!"
+            fi
+            ;;
+esac
--- playlist.py
+++ playlist.py
@@ -2,30 +2,36 @@
 '''
 AUTHOR: Reprise
 DATE: 05.18.17
+Version: 1.0.0
 
 PURPOSE:
 This program is meant to create .m3u entries to assist in the creation of playlists.
 While this program does not allow a user-specified ordering, the user can take the entries
-and order them themselves.
-
-Version: 1.0.0
+and order them themselves.  it is meant to be called from 'ytdl' which will give it a path as an argument.
+the path should be where the playlist is to be created.
 '''
 
 import os
+import sys
+import re
 from mutagen.mp3 import MP3
+
+if len(sys.argv) < 2:
+    print("Invalid path to directory.")
+    exit(0)
 
 FORMAT_DESCRIPTOR = "#EXTM3U"
 FILE_MARKER = "#EXTINF"
-PATH = '/home/reprise/Music/ytdl-downloads'
+PATH = sys.argv[1]  # This should be given by ytdl.
 filename = "DATA.m3u"
 
 playlist = open(filename, 'w')
 playlist.write(FORMAT_DESCRIPTOR + "\n\n")
-                        #os.getcwd()
+
 for track in os.listdir(PATH):
     path_track = PATH + "/" + track
 
-    #Write entry as it should appear in playlist
+    # Write entry as it should appear in playlist
     playlist.write(FILE_MARKER + ":")
 
     # Write RUNTIME
@@ -34,11 +40,12 @@ for track in os.listdir(PATH):
     playlist.write(str(runtime)[0:3])
     playlist.write("," + track + "\n")
 
-    #write path.
+    # write path.
     playlist.write(path_track + "\n")
+
+    print("entry created: " + track)
 
 playlist.close()
 
-# Left to do: The directory this program is run in should be the ROOT directory, and any music
-# Found in subdirectories should have a path. The goal was not to have absolute paths.
-
+# TODO : The directory this program is run in should be the ROOT music directory.  The goal was not to have absolute paths.
+# TODO : Find music in recursive directories, and understand their path from the root folder, being argv[1]. (walk?)
